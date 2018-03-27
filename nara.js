a4a5115502ad2c4d2596d957ea79e8ce9b8bb16b
Łukasz Sałajczyk <lukasz.salajczyk@jazzy.pro>
2018-03-27 13:28:35 +0200

    Codiras version ( ͡° ͜ʖ ͡°)

--- nara.js
+++ nara.js
@@ -1,279 +1,166 @@
 var Carousel = (function() {
     function CaruselClass(element, cfg) {
-        this.element    = element;
-        this.cfg        = cfg;
-        this.timer      = null;
-        this.isStarted  = false;
+        this.cfg        = {
+            elementAmount:  cfg.elementAmount || 2,
+            duration:       cfg.duration || 1000,
+            auto:           cfg.auto !== undefined ? cfg.auto : true,
+            navigation:     cfg.navigation || {},
+            isHorizontal:   cfg.isHorizontal === undefined ? false : cfg.isHorizontal
+        };
+        this.timer          = null;
+        this.isStarted      = false;
+        this.isMoved        = false;
+        this.elementSize    = null;
+
+        this.elements   = {
+            container:  element,
+            list:       element.querySelector('ul')
+        };
 
         this.init();
     }
 
     CaruselClass.prototype.init = function() {
-        console.log(this.element.querySelector('ul'));
-        var startPoint;
-        var endPoint;
-        this.setSettings();
-            this.element.addEventListener('mousedown', function(e){
-            if(!this.isStarted){
-                return;
-            }
-            startPoint = { x: e.clientX, y: e.clientY };
-            console.log(startPoint);
-        });
-        this.element.addEventListener('mouseup', function(e){
-            if(!this.isStarted){
-                return;
-            }
-            endPoint = { x: e.clientX,y: e.clientY };
-            console.log(endPoint);
-            this.moveBasedOnPoints(startPoint, endPoint);
-            
-        });
+        this.elementSize = this.getElementSize();
 
-        // this.start();
-    }
+        this.setSize();
+        this.addEvents();
+    };
 
-    CaruselClass.prototype.setSettings = function() {
-        var carouselHeight = this.element.clientHeight;
-        var carouselWrapper = this.element.querySelector('ul');
-        var carouselItems = this.element.querySelectorAll('li');
-        console.log(carouselWrapper);
-        carouselWrapper.style.height = carouselHeight + 'px';
-        for(var i = 0; i < carouselItems.length; i++) {
-            carouselItems[i].style.height = carouselHeight / this.cfg.items + 'px';
+    CaruselClass.prototype.getElementSize = function() {
+        var w = this.elements.container.clientWidth;
+        var h = this.elements.container.clientHeight;
+
+        if(this.cfg.isHorizontal){
+            return {
+                width:  w / this.cfg.elementAmount,
+                height: h
+            };
         }
-    }
-
-    CaruselClass.prototype.moveBasedOnPoints = function(startPoint, endPoint) {
-        var gesture = this.calcGesture(startPoint, endPoint);
-
-        // @TODO check this.cfg.vertical and gesture.isVertical | gesture.isHorizontal
-        if(gesture.isNext) {
-            return this.moveNext();
-        }
-        if(gesture.isPrev) {
-            return this.movePrev();
-        }
-    }
-
-    CaruselClass.prototype.calcGesture = function(startPoint, endPoint) {
-        // @TODO
 
         return {
-            isHorizontal: true,
-            isVertical: false,
-            isNext: true,
-            isPrev: false
+            width:  w,
+            height: h / this.cfg.elementAmount
+        };
+    };
+
+    CaruselClass.prototype.setSize = function() {
+        var items = this.elements.container.querySelectorAll('li');
+
+        if(this.cfg.isHorizontal) {
+            this.elements.list.classList.add('horizontal');
+            this.elements.list.style.width = this.elementSize.width * items.length + 'px';
         }
-    }
+
+        items.forEach(function(item){
+            item.style.width    = this.elementSize.width + 'px';
+            item.style.height   = this.elementSize.height + 'px';
+        }.bind(this));
+    };
+
+    CaruselClass.prototype.addEvents = function(){
+        if(this.cfg.navigation.prev){
+            this.cfg.navigation.prev.addEventListener('click', this.movePrev.bind(this));
+        }
+        if(this.cfg.navigation.next){
+            this.cfg.navigation.next.addEventListener('click', this.moveNext.bind(this));
+        }
+    };
 
     CaruselClass.prototype.start = function() {
-        this.timer      = setInterval(this.moveNext.bind(this), 1000);
         this.isStarted  = true;
-    }
+
+        if(this.cfg.auto) {
+            var time = Number.isInteger(this.cfg.auto) ? this.cfg.auto : 1000;
+            this.timer = setInterval(this.moveNext.bind(this), time);
+        }
+    };
 
     CaruselClass.prototype.stop = function() {
-        clearInterval(this.timer);
+        if(this.timer) {
+            clearInterval(this.timer);
+        }
 
         this.timer      = null;
         this.isStarted  = false;
-    }
+    };
 
     CaruselClass.prototype.moveNext = function() {
-        // TODO
-        console.log(lastChild);
-        var carouselWrapper = this.element.querySelector('ul');
-        var carouselItems = this.element.querySelectorAll('li');
-        var firstChild = carouselItems[0];
-        var lastChild = carouselItems[carouselItems.length];
-        carouselWrapper.insertBefore(firstChild, lastChild);
-    }
+        if(!this.isStarted){
+            return;
+        }
+        if(this.isMoved){
+            return;
+        }
+
+        this.isMoved = true;
+
+        this.moveAnimation(-this.getMainSize(), this.cfg.duration, function(){
+            this.firstItemToEnd();
+
+            this.isMoved = false;
+        }.bind(this));
+    };
 
     CaruselClass.prototype.movePrev = function() {
-        // TODO
-        var carouselWrapper = this.element.querySelector('ul');
-        var carouselItems = this.element.querySelectorAll('li');
-        var firstChild = carouselItems[0];
-        var lastChild = carouselItems[carouselItems.length];
-        carouselWrapper.insertBefore(firstChild, lastChild.nextSibling);
-    }
+        if(!this.isStarted){
+            return;
+        }
+        if(this.isMoved){
+            return;
+        }
+
+        this.isMoved = true;
+
+        this.lastItemToStart();
+        this.setListPosition(-this.getMainSize());
+
+        setTimeout(function(){
+            this.moveAnimation(0, this.cfg.duration, function(){
+                this.isMoved = false;
+            }.bind(this));
+        }.bind(this), 10);
+    };
+
+    CaruselClass.prototype.firstItemToEnd = function(){
+        var elements    = this.elements.container.querySelectorAll('li');
+        var firstElem   = elements[0];
+        var lastElem    = elements[elements.length - 1];
+
+        this.elements.list.insertBefore(firstElem, lastElem.nextSibling);
+    };
+
+    CaruselClass.prototype.lastItemToStart = function(){
+        var elements    = this.elements.container.querySelectorAll('li');
+        var firstElem   = elements[0];
+        var lastElem    = elements[elements.length - 1];
+
+        this.elements.list.insertBefore(lastElem, firstElem);
+    };
+
+    CaruselClass.prototype.setListPosition = function(value) {
+        var styleFiled  = (this.cfg.isHorizontal) ? 'marginLeft' : 'marginTop';
+
+        this.elements.list.style[styleFiled] = value + 'px';
+    };
+
+    CaruselClass.prototype.getMainSize = function() {
+        return this.cfg.isHorizontal ? this.elementSize.width : this.elementSize.height;
+    };
+
+    CaruselClass.prototype.moveAnimation = function(to, duration, onDone){
+        onDone          = onDone || function(){};
+
+        this.elements.list.style.transition = 'all ' + duration / 1000 + 's';
+        this.setListPosition(to);
+
+        setTimeout(function(){
+            this.elements.list.style.transition = 'none';
+            this.setListPosition(0);
+
+            onDone();
+        }.bind(this), duration);
+    };
 
     return CaruselClass;
 }());
-
-// var foo = new Carousel(bannerElem, {
-//     width: bannerElem.clientWidth, // Images are forced into a width of this many pixels.
-//     height: bannerCarouselContainer.clientHeight,
-//     numVisible: parseInt(bannerCarouselContainer.dataset.itemsPerPage), // The number of images visible at once.
-//     duration: 600, // Animation duration in milliseconds.
-//     padding: 2, // Vertical padding around each image, in pixels.
-//     vertical: true,
-// });
-
-// foo.start();
-
-
-
-
-
-
-
-
-
-// var Carousel = {
-//     width: bannerElem.clientWidth, // Images are forced into a width of this many pixels.
-//     height: bannerCarouselContainer.clientHeight,
-//     numVisible: parseInt(bannerCarouselContainer.dataset.itemsPerPage), // The number of images visible at once.
-//     duration: 600, // Animation duration in milliseconds.
-//     padding: 2, // Vertical padding around each image, in pixels.
-//     vertical: true,
-// };
-
-// var carousel = (Carousel.carousel = bannerCarouselWrapper),
-// images = Carousel.carousel.querySelectorAll('div'),
-// numImages = images.length,
-// imageWidth = Carousel.width,
-// padding = Carousel.padding,
-// imageHeight = Carousel.height / Carousel.numVisible;
-
-// function initCarousel() {   
-//     carousel.style.position = 'absolute';
-//     if(!Carousel.vertical){
-//         carousel.style.width = Carousel.width * numImages + 'px'; 
-//         carousel.style.display = 'flex';
-//     } else {
-//         carousel.style.width = Carousel.width + 'px';
-//         carousel.style.top = -imageHeight + 'px';
-//     }
-//     carousel.style.height = Carousel.height + 'px';
-//     carousel.style.visibility = 'visible';
-//     for(var i = 0; i < numImages; i++) {
-//         console.log(images[i]);
-//         images[i].style.height = imageHeight + 'px';
-//         images[i].style.marginBottom = padding + 'px';
-//         if(!Carousel.vertical){ 
-//             images[i].style.width = imageWidth + 'px';
-//         }
-//     }
-// }
-
-// initCarousel();
-
-// var isAnimated = false;
-
-// function animate(begin, end, finalTask) {
-//     isAnimated = true;
-//     var wrapper = Carousel.wrapper,
-//     carousel = Carousel.carousel,
-//     change = end - begin,
-//     duration = Carousel.duration,
-//     startTime = Date.now();
-//     carousel.style.top = begin + 'px';
-
-//     var animateInterval = window.setInterval(function() {
-//         var t = Date.now() - startTime;
-//         if (t >= duration) {
-//                 window.clearInterval(animateInterval);
-//                 finalTask();
-//                 isAnimated = false;
-//                 return;
-//             }
-//         t /= duration / 2;
-//         console.log(t);
-//         var top =
-//             begin +
-//             (t < 1
-//             ? change / 2 * Math.pow(t, 3)
-//             : change / 2 * (Math.pow(t - 2, 3) + 2));
-//         carousel.style.top = top + 'px';
-//         },
-//     1000 / 60);
-// }
-
-//     function animateHorizontal(begin, end, finalTask) {
-//         isAnimated = true;
-//         var wrapper = Carousel.wrapper,
-//         carousel = Carousel.carousel,
-//         change = end - begin,
-//         duration = Carousel.duration,
-//         startTime = Date.now();
-//         carousel.style.right = begin + 'px';
-
-//         var animateInterval = window.setInterval(function() {
-//             var t = Date.now() - startTime;
-//             if (t >= duration) {
-//                     window.clearInterval(animateInterval);
-//                     finalTask();
-//                     isAnimated = false;
-//                     return;
-//                 }
-//             t /= duration / 2;
-//             console.log(t);
-//             var right =
-//                 begin +
-//                 (t < 1
-//                 ? change / 2 * Math.pow(t, 3)
-//                 : change / 2 * (Math.pow(t - 2, 3) + 2));
-//             carousel.style.right = right + 'px';
-//             }, 1000 / 60);
-//     }
-
-//   var autoLoop = window.setInterval(animatedLoop, 2000);
-
-  
-//   function animatedLoop() {
-//     if(!Carousel.vertical) {
-//         animateHorizontal(0, -imageWidth, function() {
-//             rotateBackward();
-//             carousel.style.right = -imageWidth + 'px';
-//         });
-//     } else {
-//         animate(-imageHeight, -imageHeight * 2, function() {
-//             rotateBackward();
-//             carousel.style.top = -imageHeight + 'px';
-//         });
-//     }
-// }
-
-// function rotateForward() {
-//     var carousel = Carousel.carousel,
-//     children = carousel.children,
-//     firstChild = children[0],
-//     lastChild = children[children.length - 1];
-//     carousel.insertBefore(lastChild, firstChild);
-// }
-
-// function rotateBackward() {
-//     var carousel = Carousel.carousel,
-//     children = carousel.children,
-//     firstChild = children[0],
-//     lastChild = children[children.length - 1];
-//     carousel.insertBefore(firstChild, lastChild.nextSibling);
-// }
-
-// var touchStartY = null;
-
-// carousel.addEventListener('mousedown', function(e) {
-// window.clearInterval(autoLoop);
-// touchStartY = e.clientY;
-// });
-
-// carousel.addEventListener('mouseup', function(e) {
-// var touchLength = Math.abs(touchStartY - e.clientY);
-// if (!isAnimated) {
-//     if (touchStartY > e.clientY && touchLength > 10) {
-//     animate(-imageHeight, -imageHeight * 2, function() {
-//         rotateBackward();
-//         carousel.style.top = -imageHeight + 'px';
-//     });
-//     }
-//     if (touchStartY < e.clientY && touchLength > 10) {
-//     rotateForward();
-//     animate(-imageHeight * 2, -imageHeight, function() {
-//         carousel.style.top = -imageHeight + 'px';
-//     });
-//     }
-// }
-
-// autoLoop = window.setInterval(animatedLoop, 2000);
-// });
--- script.js
+++ script.js
@@ -1,45 +1,12 @@
-var bannerElem = document.querySelector('.banner'),
-    bannerDatasets = bannerElem.querySelectorAll('[data-field-name]'),
-    bannerCarouselContainer = document.querySelector('.banner-carouselContainer'),
-    numberPerPage = parseInt(bannerCarouselContainer.dataset.itemsPerPage),
-    bannerCarouselWrapper = document.querySelector('.banner-carouselWrapper');
+var elem = document.querySelector('.carousel');
 
-    // bannerElem.addEventListener('click', () => {
-    //     window.open(product.Url, '_blank');
-    // })
-
-function addLogo() {
-    bannerElem.querySelector('.logo').style.background = "url('local-images/logo.png') no-repeat 50% 50%";
-    bannerElem.querySelector('.logo').style.backgroundSize = "44px";
-}
-
-addLogo();
-
-function includeImages() {
-
-        for(var i = 1; i <= 5; i++) {
-            var li = document.createElement('li');
-            li.style.background = 'url(./images/' + i + '.jpg) no-repeat 50% 50%';
-            // div.style.width = '100%';
-            bannerCarouselWrapper.appendChild(li)
-        }
-
-        var banner = new Carousel(bannerCarouselContainer, {
-            isHorizontal: true,
-            items: numberPerPage
-        })
-
-        // var mySiema = new Siema({
-        //     selector: '.siema',
-        //     duration: 200,
-        //     perPage: 1,
-        //     loop: true,
-        //     draggable: true,
-        // });
-
-        // setInterval(function() {
-        //     mySiema.next()
-        // }, 2500)
-    }
-
-includeImages();
+new Carousel(elem, {
+    elementAmount: 3,
+    duration: 300,
+    auto: false, // [true, 2000]
+    navigation: {
+        prev: document.querySelector('.prev'),
+        next: document.querySelector('.next')
+    },
+    isHorizontal: false
+}).start();
